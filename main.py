c022f997539ae740c2eca112e73afcac2043736c
Ian147 <31514987+Ian147@users.noreply.github.com>
2025-03-01 10:07:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import logging
 from trading_bot import TradingBot
 
 def main():
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-    logging.info("🚀 Memulai bot trading...")
-
-    # Inisialisasi bot dengan simbol dan timeframe
-    bot = TradingBot(symbol="BTCUSDT", timeframe="1m")
+    bot = TradingBot(symbol="BTCUSDT", timeframe="1h")
     bot.run()
 
 if __name__ == "__main__":
