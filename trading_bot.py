9f04a4732b46ad3a5e5959668c2d6e719ec09958
Ian147 <31514987+Ian147@users.noreply.github.com>
2025-03-03 00:51:31 +0800

    Update trading_bot.py

--- trading_bot.py
+++ trading_bot.py
@@ -32,8 +32,8 @@ binance = ccxt.binance({
 # Pair yang diperdagangkan
 symbol = "BTC/USDT"
 trade_amount = 10  # Order 10 USDT per transaksi
-tp_percentage = 5 / 100  # TP +5%
-sl_percentage = 5 / 100    # SL -5%
+tp_percentage = 60 / 100  # TP +60%
+sl_percentage = 50 / 100    # SL -50%
 
 # Fungsi Kirim Notifikasi ke Telegram
 def send_telegram_message(message):
