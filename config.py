4388f64dd993973774f3f8c3aa3e8178d92bafe5
Ian147 <31514987+Ian147@users.noreply.github.com>
2025-03-05 10:29:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-# config.py
 BINANCE_API_KEY = "4DOHVQeYb9nniTvmykIhEfVqdop2jcC0ibSMa3vaxYnjRH4VxBrre7AN5ZMxKyk9"
 BINANCE_API_SECRET = "R4h62y9sdFJyLfeDtTRCDUX8ou6njtxRoopLFWpqUkhFIMTtMCteNg8OPNp4Y8Pl"
 TELEGRAM_BOT_TOKEN = "8011128170:AAEvCJrvMRinnIsInJmqLjzpWguz88tPWVw"
 TELEGRAM_CHAT_ID = "681125756"
-PAIR = "BTCUSDT"  # Pasangan trading
-TIMEFRAME = "15m" # Timeframe untuk prediksi
+
+PAIR = "BTCUSDT"
+TIMEFRAME = "15m"
 TRADE_AMOUNT_USDT = 10  # USDT per order
 TP_PERCENT = 1.5  # Take Profit +1.5%
 SL_PERCENT = 5  # Stop Loss -5%
+DATA_PATH = "data.csv"  # Lokasi penyimpanan data historis
+MODEL_PATH = "lstm_model.h5"  # Lokasi penyimpanan model
+SCALER_PATH = "scaler.npy"  # Lokasi penyimpanan scaler
